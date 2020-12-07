0a78e0e55374be18a09a2c74c806a514de5dc230
Johannes Borrmann <j.borrmann@resolution.de>
2020-12-07 16:38:38 +0100

    Added locust based tests for HTTP Header Auth

--- app/extension/confluence/extension_locust.py
+++ app/extension/confluence/extension_locust.py
@@ -6,23 +6,21 @@ logger = init_logger(app_type='confluence')
 
 @confluence_measure
 def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
+
+    # add header key and value for http header auth to log us in
+    current_user = locust.session_data_storage["username"]
+
+    # execute a GET request
+    r = locust.get('/rest/api/user/current', headers={"X-AUTH": current_user}, catch_response=True)
     content = r.content.decode('utf-8')   # decode response content
 
-    token_pattern_example = '"token":"(.+?)"'
-    id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
+    # expecting the same username in the response
+    username_pattern = '"username":"(.+?)"'
+    username_for_assertion = re.findall(username_pattern, content)
 
-    logger.locust_info(f'token: {token}, id: {id}')  # log information for debug when verbose is true in jira.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
+    logger.locust_info(f'username from /rest/api/user/current response: {username_for_assertion[0]}')
 
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
-    content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+    if username_for_assertion[0] != current_user:
+        logger.error(f" username in response not found/ not matching the current username")
+
+    assert username_for_assertion[0] == current_user
