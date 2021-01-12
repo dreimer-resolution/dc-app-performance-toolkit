d451957be77d466b9aa6d3837e292cfc496289a7
Johannes Borrmann <j.borrmann@resolution.de>
2021-01-12 16:08:13 +0100

    Added test for Vanity URL aka Link Shortener

--- app/extension/confluence/extension_locust.py
+++ app/extension/confluence/extension_locust.py
@@ -1,28 +1,52 @@
 import re
 from locustio.common_utils import init_logger, confluence_measure
+import time
 
 logger = init_logger(app_type='confluence')
+
+"""
+This test requires a space called ISS with a page called Crew Section
+"""
 
 
 @confluence_measure
 def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
-    content = r.content.decode('utf-8')   # decode response content
 
-    token_pattern_example = '"token":"(.+?)"'
-    id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
+    random_short_url =  str(int(round(time.time() * 1000)))
 
-    logger.locust_info(f'token: {token}, id: {id}')  # log information for debug when verbose is true in jira.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
+    r = locust.get('/', catch_response=True)
+    base_url = r.url
 
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
+    # create a short URL
+    null = None
+    body = {
+        "id": null,
+        "shortUrl": random_short_url,
+        "longUrl": base_url + "/display/ISS/Crew+Section",
+        "note": "Meet the Crew",
+        "anchor": null
+    }
+    r = locust.post('/rest/vanityurl-admin/2.0/urlmappings', json=body, catch_response=True)
     content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+
+    if 'creationDate' not in content:
+        logger.error(f"creationDate was not found in {content}, looks like the short URL was not created")
+
+    assert 'creationDate' in content  # assert if plain text token is contained in response
+
+    short_url_pattern = '"shortUrl":"(.+?)"'
+    short_url_from_result = re.findall(short_url_pattern, content)
+
+    creation_date_from_result_pattern = '"creationDate":"(.+?)"'
+    creation_date_from_result = re.findall(creation_date_from_result_pattern, content)
+
+    logger.locust_info(f"created shortUrl: {short_url_from_result[0]}, creationDate is {creation_date_from_result[0]}")
+
+
+    # request short URL
+    r = locust.get('/go/' + short_url_pattern, catch_response=True)
+
+    if r.status_code != 200:
+        logger.error(f"calling the shortUrl {short_url_from_result[0]} didn't return status 200")
+
+    assert r.status_code == 200
