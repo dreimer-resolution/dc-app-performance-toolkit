9a21330fc52c211756b3421a11c4965538f970b3
Johannes Borrmann <j.borrmann@resolution.de>
2021-01-20 15:24:13 +0100

    Added User Deactivator locust tests for Jira and Confluence. These are triggering a bulk deactivation of users not in the confluence-users or jira-software-users group.

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -6,23 +6,21 @@ logger = init_logger(app_type='jira')
 
 @jira_measure
 def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
-    content = r.content.decode('utf-8')   # decode response content
 
-    token_pattern_example = '"token":"(.+?)"'
-    id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
+    # this will trigger deactivation of everybody who is not in jira-software-users,
+    # like our test users created with the test connector
+    body = {"notInGroups": ["jira-software-users"], "action": "DEACTIVATE"}
 
-    logger.locust_info(f'token: {token}, id: {id}')  # log information for debug when verbose is true in confluence.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
-
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
+    r = locust.post('/rest/de.resolution.userdeactivator/1.0/ui/users', json=body, catch_response=True)
     content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+
+    if 'resultId' not in content:
+        logger.error(f"resultId was not found in {content}, looks like deactivation was not triggered properly")
+
+    assert 'resultId' in content
+
+    result_id_pattern = '"resultId":(.+?)'
+    result_id = re.findall(result_id_pattern, content)
+
+    logger.locust_info(f"resultId: {result_id[0]}")
+    assert int(result_id[0]) >= 0
--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -7,19 +7,63 @@ from selenium_ui.conftest import print_timing
 from util.conf import JIRA_SETTINGS
 
 
+"""
+This is just an example of how to use UD via selenium, could be used for SSO later.
+This type of test is only applicable for actions which should be performed by every test user.
+It's pointless to let 200 test users deactivate thousands of other users at the same time. 
+
+Comment from Atlassian:
+
+there are a bunch of ways to do that. As you mentioned:
+ - Locust test with low concurrency
+ - Run long running task manually during the test run (e.g. with REST)
+ - Modify selenium test with a logic (if possible) - do not do long running task if it is in progress
+    - Use dataset object in selenium test as a flag to start long running task in app-specific test one time. Something like:
+
+def app_specific_action(webdriver, datasets):
+    @print_timing("selenium_app_custom_action")
+    def measure():
+        @print_timing("selenium_app_custom_action:view_page")
+        def sub_measure():
+            if "app_specific_flag" not in datasets.keys():
+                print("Do one time specific thing")
+                datasets["app_specific_flag"] = True
+        sub_measure()
+    measure()
+
+"""
+
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        app_specific_issue = random.choice(datasets['custom_issues'])
-        issue_key = app_specific_issue[0]
+
 
     @print_timing("selenium_app_custom_action")
     def measure():
 
-        @print_timing("selenium_app_custom_action:view_issue")
+        @print_timing("selenium_app_custom_action:user_bulk_operation")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/userdeactivator/admin") # open bulk user page
+
+            # Wait for filter button to appear, indicating users have been loaded
+            page.wait_until_visible((By.XPATH, ".//span[text()='Filter']"))
+
+            # Check first check box, selecting all users
+            webdriver.find_element_by_xpath(".//input[@type='checkbox']")[0].click
+            # press bulk operation button
+            webdriver.find_element_by_xpath(".//button[span='Choose Bulk Action']")[0].click
+            # select deactivation
+            webdriver.find_element_by_xpath(".//input[@type='radio' and @value='1']")[0].click()
+            # click next
+            webdriver.find_element_by_xpath(".//button[span='Next']")[0].click()
+            # confirm
+            webdriver.find_element_by_xpath(".//button[span='Confirm']")[0].click()
+
+            # Wait for operation to complete
+            page.wait_until_visible((By.XPATH, ".//span[text()='Operation Done!']"))
+
+            # close button
+            webdriver.find_element_by_xpath(".//button[span='Close']")[0].click()
+
+
         sub_measure()
     measure()
