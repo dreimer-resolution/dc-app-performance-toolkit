5dec6d50738e23637ebc30195448321aae60ee87
Johannes Borrmann <j.borrmann@resolution.de>
2021-01-26 16:14:50 +0100

    Added sample code which fixes the authentication for admins on the REST API (which seems to be a weird solution, hence only a sample)

--- app/extension/confluence/extension_locust.py
+++ app/extension/confluence/extension_locust.py
@@ -3,6 +3,34 @@ from locustio.common_utils import init_logger, confluence_measure
 
 logger = init_logger(app_type='confluence')
 
+"""
+
+    login_body = {
+        'os_username': '',
+        'os_password': '',
+        'os_cookie': True,
+        'os_destination': '',
+        'login': 'Log in'
+    }
+    TEXT_HEADERS = {
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    }
+    login_body['os_username'] = 'admin'
+    login_body['os_password'] = 'admin'
+    locust.post('/dologin.action', login_body, TEXT_HEADERS, catch_response=True)
+    locust.post('/doauthenticate.action', auth=('admin', 'admin'), json={'authenticate':'Confirm', 'password': 'admin'}, catch_response=True )
+    r = locust.post('/rest/de.resolution.userdeactivator/1.0/ui/users', json={"notInGroups":["confluence-users"],"action":"ACTIVATE"}, headers={'content-type': 'application/json', 'x-experimentalapi':'opt-in'}, catch_response=True)  # call app-specific GET endpoint
+    login_body['os_username'] = locust.session_data_storage['username']
+    login_body['os_password'] = 'password'
+    locust.post('/dologin.action', login_body, TEXT_HEADERS, catch_response=True)
+    r = locust.post('/rest/de.resolution.userdeactivator/1.0/ui/users', json={"notInGroups":["confluence-users"],"action":"ACTIVATE"}, headers={'content-type': 'application/json', 'x-experimentalapi':'opt-in'}, catch_response=True)  # call app-specific GET endpoint
+
+"""
+
+
 
 @confluence_measure("locust_app_specific_action")
 def app_specific_action(locust):
