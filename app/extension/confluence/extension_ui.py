331c0661aa8681d27ea99d930af5f6e7a2072add
Johannes Borrmann <j.borrmann@resolution.de>
2021-02-10 11:50:58 +0100

    Added Confluence app specific login

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -6,19 +6,38 @@ from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
 from util.conf import CONFLUENCE_SETTINGS
 
+"""
+https://social.technet.microsoft.com/wiki/contents/articles/24541.powershell-bulk-create-ad-users-from-csv-file.aspx
+https://www.alitajran.com/create-active-directory-users-from-csv-with-powershell/
+"""
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_pages']:
-        app_specific_page_id = datasets['custom_page_id']
+
 
     @print_timing("selenium_app_custom_action")
     def measure():
 
-        @print_timing("selenium_app_custom_action:view_page")
+        @print_timing("selenium_app_custom_action:login_with_open_id")
         def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
-            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            print(f"login_with_open_id, user: {datasets['username'][0:20]}")
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/login.action") # open dashboard page with login screen
+
+            page.wait_until_visible((By.ID, "openid-1"))
+
+            webdriver.find_element_by_xpath(".//*[@id='openid-1']").click()
+
+            page.wait_until_visible((By.ID, "userNameInput"))
+            page.wait_until_visible((By.ID, "passwordInput"))
+
+            username_input = webdriver.find_element_by_xpath(".//*[@id='userNameInput']")
+            username_input.send_keys("ad\\" + datasets['username'][0:20])
+
+            password_input = webdriver.find_element_by_xpath(".//*[@id='passwordInput']")
+            password_input.send_keys('just4lab!')
+
+            webdriver.find_element_by_xpath(".//*[@id='submitButton']").click()
+
+
         sub_measure()
     measure()
