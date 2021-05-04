51b0607c7b70882f10a7e366f049f8c2ac7609da
Johannes Borrmann <j.borrmann@resolution.de>
2021-05-04 12:44:20 +0200

    Added Confluence User Sync test

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -1,5 +1,4 @@
-import random
-
+import time
 from selenium.webdriver.common.by import By
 
 from selenium_ui.base_page import BasePage
@@ -10,35 +9,45 @@ from util.conf import CONFLUENCE_SETTINGS
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_pages']:
-        app_specific_page_id = datasets['custom_page_id']
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.wait_for_page_loaded()
-    #         login_page.set_credentials(username=username, password=password)
-    #         login_page.click_login_button()
-    #         if login_page.is_first_login():
-    #             login_page.first_user_setup()
-    #         all_updates_page = AllUpdates(webdriver)
-    #         all_updates_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    @print_timing("selenium_app_specific_user_login")
+    def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.wait_for_page_loaded()
+            login_page.set_credentials(username=username, password=password)
+            login_page.click_login_button()
+            if login_page.is_first_login():
+                login_page.first_user_setup()
+            all_updates_page = AllUpdates(webdriver)
+            all_updates_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+    measure()
 
     @print_timing("selenium_app_custom_action")
     def measure():
 
-        @print_timing("selenium_app_custom_action:view_page")
+        @print_timing("selenium_app_custom_action:trigger_usersync")
         def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
-            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            # open user sync connector page
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/plugins/servlet/de.resolution.usersync/admin")
+            # 1 sec pause to prevent 500 errors, obviously caused by trying to use the UI too quickly during automation
+            time.sleep(1)
+            # wait for table to be loaded
+            page.wait_until_visible((By.ID, 'usersync-connector-table-body'))
+            # check if there is a sync running already
+            running_sync = webdriver.find_elements_by_xpath(".//span[.='Sync is running...']")
+            # if not
+            if len(running_sync) == 0:
+                # another check to see if sync button is now available
+                new_sync = webdriver.find_elements_by_xpath(".//span[.='Sync']")
+                if len(new_sync) > 0:
+                    # click on sync
+                    webdriver.find_element_by_xpath(".//span[.='Sync']").click()
+                    # wait for status window
+                    page.wait_until_visible((By.ID, 'usersync-sync-status-dialog'))
+                    # we are done here
         sub_measure()
     measure()
