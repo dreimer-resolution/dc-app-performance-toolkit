f3ae97a3286689c214622ddbfc1574c36d29b07f
Johannes Borrmann <j.borrmann@resolution.de>
2021-12-28 10:28:39 +0100

    Added test for Jira version of the custom charts app

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -10,8 +10,6 @@ from util.conf import JIRA_SETTINGS
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
 
     # To run action as specific user uncomment code bellow.
     # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
@@ -34,11 +32,11 @@ def app_specific_action(webdriver, datasets):
 
     @print_timing("selenium_app_custom_action")
     def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
+        @print_timing("selenium_app_custom_action:open_dashboard_with_gadget")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/Dashboard.jspa?selectPageId=10100")
+            # page.wait_until_visible((By.ID, "ossa-chart-wrapper"))
+            page.wait_until_visible((By.CLASS_NAME, "chart-footer"))
         sub_measure()
     measure()
 
