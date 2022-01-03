957a59ab5fb65a1d65ac1e40463096d5ebf7d03b
Johannes Borrmann <j.borrmann@resolution.de>
2022-01-03 19:49:02 +0100

    Added Jira test

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -10,8 +10,6 @@ from util.conf import JIRA_SETTINGS
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
 
     # To run action as specific user uncomment code bellow.
     # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
@@ -36,9 +34,10 @@ def app_specific_action(webdriver, datasets):
     def measure():
         @print_timing("selenium_app_custom_action:view_issue")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            # open an issue where the s3 list is included
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/<ISSUE_KEY_HERE>")
+            # wait 4 link to text file called org.hotovo.confluence.amazon-s3-storage-connector.txt in s3 connector list
+            page.wait_until_visible((By.XPATH, "//*[@title='org.hotovo.confluence.amazon-s3-storage-connector.txt']"))
         sub_measure()
     measure()
 
