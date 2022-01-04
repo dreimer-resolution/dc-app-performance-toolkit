935e4df651fcbea2308876aba98e797909819045
Johannes Borrmann <j.borrmann@resolution.de>
2022-01-04 15:47:52 +0100

    First version of page view tracker test

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -13,32 +13,33 @@ def app_specific_action(webdriver, datasets):
     if datasets['custom_pages']:
         app_specific_page_id = datasets['custom_page_id']
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.wait_for_page_loaded()
-    #         login_page.set_credentials(username=username, password=password)
-    #         login_page.click_login_button()
-    #         if login_page.is_first_login():
-    #             login_page.first_user_setup()
-    #         all_updates_page = AllUpdates(webdriver)
-    #         all_updates_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    @print_timing("selenium_app_specific_user_login")
+    def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.wait_for_page_loaded()
+            login_page.set_credentials(username=username, password=password)
+            login_page.click_login_button()
+            if login_page.is_first_login():
+                login_page.first_user_setup()
+            all_updates_page = AllUpdates(webdriver)
+            all_updates_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+    measure()
 
     @print_timing("selenium_app_custom_action")
     def measure():
 
-        @print_timing("selenium_app_custom_action:view_page")
+        @print_timing("selenium_app_custom_action:open_page_view_statistics")
         def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
-            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            # open page with a page view statistics macro embedded
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/display/MYS/OSS")
+            page.wait_until_visible((By.XPATH,
+                                     "//canvas[starts-with(@class, 'pvt-chart-macro chartjs-render-monitor')]"))
+
+
+
         sub_measure()
     measure()
