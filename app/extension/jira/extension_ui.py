66a64f9f5f690cfb4aef8874bfd754bbc4e93dd3
Johannes Borrmann <jobo@newcube.fritz.box>
2022-01-25 15:44:40 +0100

    Adjusted OpenID test for Jira so that it works with Azure AD.

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -1,67 +1,75 @@
-import random
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
-from selenium_ui.jira.pages.pages import Login
-from util.conf import JIRA_SETTINGS
+from selenium.common.exceptions import TimeoutException
 from selenium_ui.jira.pages.pages import Login, PopupManager, Issue, Project, Search, ProjectsList, \
     BoardsList, Board, Dashboard, Logout
 
-from selenium_ui.jira.pages.selectors import UrlManager, LoginPageLocators, DashboardLocators, PopupLocators, \
-    IssueLocators, ProjectLocators, SearchLocators, BoardsListLocators, BoardLocators, LogoutLocators
+"""
+Prerequisites: 
+- changes performance users email address to ...@azuread.lab.resolution.de (Excel Connector)
+- Grant admin consent to user permissions in app registration (this is done already for https://portal.azure.com/#blade/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/CallAnAPI/appId/2beed0fd-1a83-42c9-8ebb-3be6d4192bd2/isMSAApp/) 
+"""
 
-"""
-https://social.technet.microsoft.com/wiki/contents/articles/24541.powershell-bulk-create-ad-users-from-csv-file.aspx
-https://www.alitajran.com/create-active-directory-users-from-csv-with-powershell/
-"""
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
-
     @print_timing("selenium_app_custom_action")
     def measure():
-        @print_timing("selenium_app_custom_action:login_with_open_id")
-        def sub_measure():
-            print(f"login_with_open_id, user: {datasets['username'][0:20]}")
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/Dashboard.jspa") # open dashboard page with login screen
 
+        login_page = Login(webdriver)
+
+        @print_timing("selenium_app_specific_login:open_login_page")
+        def sub_measure():
+            login_page.go_to()
+        sub_measure()
+
+        @print_timing("selenium_app_custom_action:login_with_open_id_and_view_dashboard")
+        def sub_measure():
+            print(f"login_with_open_id, user: {datasets['username']}")
+
+            # click on open-id login button
             page.wait_until_visible((By.ID, "openid-1"))
             webdriver.find_element_by_xpath(".//*[@id='openid-1']").click()
 
-            page.wait_until_visible((By.ID, "userNameInput"))
-            page.wait_until_visible((By.ID, "passwordInput"))
+            # wait for azure user input field to be shown
+            page.wait_until_visible((By.ID, "i0116"))
+            # get username field
+            username_input = webdriver.find_element_by_xpath(".//*[@id='i0116']")
+            # clear existing value
+            username_input.clear()
+            # add username to it
+            username_input.send_keys(datasets['username'] + "@azuread.lab.resolution.de")
+            next_is_password = webdriver.find_element_by_xpath(".//*[@id='idSIButton9']")
+            next_is_password.click()
 
-            username_input = webdriver.find_element_by_xpath(".//*[@id='userNameInput']")
-            username_input.send_keys("ad\\" + datasets['username'][0:20])
+            try:
+                # if we don't see password input within 5 seconds ...
+                page.wait_until_visible((By.ID, "i0118"), 5)
+            except TimeoutException:
+                # ... restart test
+                app_specific_action(webdriver, datasets)
+                return
 
-            password_input = webdriver.find_element_by_xpath(".//*[@id='passwordInput']")
-            password_input.send_keys('just4lab!')
+            password_input = webdriver.find_element_by_xpath(".//*[@id='i0118']")
+            password_input.clear()
 
-            webdriver.find_element_by_xpath(".//*[@id='submitButton']").click()
-            #PopupManager(webdriver).dismiss_default_popup()
+            # this is required to prevent StaleElementReferenceException
+            actions = ActionChains(webdriver)
+            actions.send_keys("justAnotherPassw0rd!")
+            actions.send_keys(Keys.ENTER)
+            actions.perform()
 
+            stay_signed_in_no = webdriver.find_element_by_xpath(".//*[@id='idBtn_Back']")
+            stay_signed_in_no.click()
+
+            # wait for html body id "jira" which is always present, both for users who never logged in and who did
+            page.wait_until_visible((By.ID, "jira"))
+
+            """
             if page.get_elements(LoginPageLocators.continue_button):
                 page.wait_until_visible(LoginPageLocators.continue_button).send_keys(Keys.ESCAPE)
                 page.get_element(LoginPageLocators.continue_button).click()
@@ -71,10 +79,18 @@ def app_specific_action(webdriver, datasets):
                 page.wait_until_visible(DashboardLocators.dashboard_window)
             else:
                 page.wait_until_visible((By.ID, "gadget-10002-title"))
+            """
+
+            if login_page.is_first_login():
+                login_page.first_login_setup()
+            if login_page.is_first_login_second_page():
+                login_page.first_login_second_page_setup()
+            login_page.wait_for_page_loaded()
+            webdriver.node_id = login_page.get_node_id()
+            print(f"node_id:{webdriver.node_id}")
 
         sub_measure()
 
-        PopupManager(webdriver).dismiss_default_popup()
-
     measure()
+    PopupManager(webdriver).dismiss_default_popup()
 
