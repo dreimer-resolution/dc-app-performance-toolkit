64998507aadd1c41a326d257a9b8224dc66fe7ed
Johannes Borrmann <jobo@newcube.fritz.box>
2022-01-25 17:54:37 +0100

    …

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -26,62 +26,59 @@ def app_specific_action(webdriver, datasets):
         @print_timing("selenium_app_custom_action:login_with_open_id_and_view_dashboard")
         def sub_measure():
             print(f"login_with_open_id, user: {datasets['username']}")
+            # try:
+            #     # this is only present if user is logged in already, should rarely be the case
+            #     webdriver.find_element_by_xpath(".//*[@id='user-options-content']")
+            # except: # if not, there is an excption and we need to login     # noqa E722
+
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/Dashboard.jspa")
+
+            # click on open-id login button
+            page.wait_until_visible((By.ID, "openid-1"))
+            webdriver.find_element_by_xpath(".//*[@id='openid-1']").click()
+
+            # wait for azure user input field to be shown
+            page.wait_until_visible((By.ID, "i0116"))
+            # get username field
+            username_input = webdriver.find_element_by_xpath(".//*[@id='i0116']")
+            # clear existing value
+            username_input.clear()
+            # add username to it
+            username_input.send_keys(datasets['username'] + "@azuread.lab.resolution.de")
+            next_is_password = webdriver.find_element_by_xpath(".//*[@id='idSIButton9']")
+            next_is_password.click()
+
             try:
-                # this is only present if user is logged in already, should rarely be the case
-                webdriver.find_element_by_xpath(".//*[@id='user-options-content']")
-            except: # if not, there is an excption and we need to login     # noqa E722
+                # if we don't see password input within 5 seconds ...
+                page.wait_until_visible((By.ID, "i0118"), 5)
+            except TimeoutException:
+                # ... restart test
+                app_specific_action(webdriver, datasets)
+                return
 
-                page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/Dashboard.jspa")
+            password_input = webdriver.find_element_by_xpath(".//*[@id='i0118']")
+            password_input.clear()
 
-                # click on open-id login button
-                page.wait_until_visible((By.ID, "openid-1"))
-                webdriver.find_element_by_xpath(".//*[@id='openid-1']").click()
+            # this is required to prevent StaleElementReferenceException
+            actions = ActionChains(webdriver)
+            actions.send_keys("justAnotherPassw0rd!")
+            actions.send_keys(Keys.ENTER)
+            actions.perform()
 
-                # wait for azure user input field to be shown
-                page.wait_until_visible((By.ID, "i0116"))
-                # get username field
-                username_input = webdriver.find_element_by_xpath(".//*[@id='i0116']")
-                # clear existing value
-                username_input.clear()
-                # add username to it
-                username_input.send_keys(datasets['username'] + "@azuread.lab.resolution.de")
-                next_is_password = webdriver.find_element_by_xpath(".//*[@id='idSIButton9']")
-                next_is_password.click()
+            stay_signed_in_no = webdriver.find_element_by_xpath(".//*[@id='idBtn_Back']")
+            stay_signed_in_no.click()
 
-                try:
-                    # if we don't see password input within 5 seconds ...
-                    page.wait_until_visible((By.ID, "i0118"), 5)
-                except TimeoutException:
-                    # ... restart test
-                    app_specific_action(webdriver, datasets)
-                    return
-
-                password_input = webdriver.find_element_by_xpath(".//*[@id='i0118']")
-                password_input.clear()
-
-                # this is required to prevent StaleElementReferenceException
-                actions = ActionChains(webdriver)
-                actions.send_keys("justAnotherPassw0rd!")
-                actions.send_keys(Keys.ENTER)
-                actions.perform()
-
-                stay_signed_in_no = webdriver.find_element_by_xpath(".//*[@id='idBtn_Back']")
-                stay_signed_in_no.click()
-
-                # wait for html body id "jira" which is always present, both for users who never logged in and who did
-                page.wait_until_visible((By.ID, "jira"))
-
-                """
-                if page.get_elements(LoginPageLocators.continue_button):
-                    page.wait_until_visible(LoginPageLocators.continue_button).send_keys(Keys.ESCAPE)
-                    page.get_element(LoginPageLocators.continue_button).click()
-                    page.wait_until_visible(LoginPageLocators.avatar_page_next_button).click()
-                    page.wait_until_visible(LoginPageLocators.explore_current_projects).click()
-                    page.go_to_url(DashboardLocators.dashboard_url)
-                    page.wait_until_visible(DashboardLocators.dashboard_window)
-                else:
-                    page.wait_until_visible((By.ID, "gadget-10002-title"))
-                """
+            """
+            if page.get_elements(LoginPageLocators.continue_button):
+                page.wait_until_visible(LoginPageLocators.continue_button).send_keys(Keys.ESCAPE)
+                page.get_element(LoginPageLocators.continue_button).click()
+                page.wait_until_visible(LoginPageLocators.avatar_page_next_button).click()
+                page.wait_until_visible(LoginPageLocators.explore_current_projects).click()
+                page.go_to_url(DashboardLocators.dashboard_url)
+                page.wait_until_visible(DashboardLocators.dashboard_window)
+            else:
+                page.wait_until_visible((By.ID, "gadget-10002-title"))
+            """
 
             if login_page.is_first_login():
                 login_page.first_login_setup()
