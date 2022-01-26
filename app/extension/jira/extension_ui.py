f2e8b50d4406885564d71ab16185ec69a27cd302
Johannes Borrmann <jobo@newcube.fritz.box>
2022-01-26 12:17:11 +0100

    Only one test for login now, no debug logging for now

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -18,87 +18,83 @@ Prerequisites:
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
 
-    @print_timing("selenium_app_custom_action")
+    @print_timing("selenium_app_custom_action:login_with_open_id_and_view_dashboard")
     def measure():
 
         login_page = Login(webdriver)
         login_page.delete_all_cookies()
 
-        @print_timing("selenium_app_custom_action:login_with_open_id_and_view_dashboard")
-        def sub_measure():
-            print(f"starting login_with_open_id, user: {datasets['username']}")
+        print(f"starting login_with_open_id, user: {datasets['username']}")
 
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/Dashboard.jspa")
-            print(f"opened dashboard page for user: {datasets['username']}")
-            # click on open-id login button
-            page.wait_until_visible((By.ID, "openid-1"))
-            webdriver.find_element_by_xpath(".//*[@id='openid-1']").click()
-            print(f"clicked open id button for user: {datasets['username']}")
+        page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/Dashboard.jspa")
+        print(f"opened dashboard page for user: {datasets['username']}")
+        # click on open-id login button
+        page.wait_until_visible((By.ID, "openid-1"))
+        webdriver.find_element_by_xpath(".//*[@id='openid-1']").click()
+        print(f"clicked open id button for user: {datasets['username']}")
 
-            # wait for azure user input field to be shown
-            page.wait_until_visible((By.ID, "i0116"))
-            # get username field
-            username_input = webdriver.find_element_by_xpath(".//*[@id='i0116']")
-            # clear existing value
-            username_input.clear()
-            # add username to it
-            username_input.send_keys(datasets['username'] + "@azuread.lab.resolution.de")
-            next_is_password = webdriver.find_element_by_xpath(".//*[@id='idSIButton9']")
-            next_is_password.click()
+        # wait for azure user input field to be shown
+        page.wait_until_visible((By.ID, "i0116"))
+        # get username field
+        username_input = webdriver.find_element_by_xpath(".//*[@id='i0116']")
+        # clear existing value
+        username_input.clear()
+        # add username to it
+        username_input.send_keys(datasets['username'] + "@azuread.lab.resolution.de")
+        next_is_password = webdriver.find_element_by_xpath(".//*[@id='idSIButton9']")
+        next_is_password.click()
 
-            try:
-                # if we don't see password input within 5 seconds ...
-                page.wait_until_visible((By.ID, "i0118"), 2)
-                print(f"no password prompt from azure - about to restart test for user: {datasets['username']}")
-            except TimeoutException:
-                # ... restart test
-                app_specific_action(webdriver, datasets)
-                return
+        try:
+            # if we don't see password input within 5 seconds ...
+            page.wait_until_visible((By.ID, "i0118"), 1)
+            print(f"no password prompt from azure - about to restart test for user: {datasets['username']}")
+        except TimeoutException:
+            # ... restart test
+            app_specific_action(webdriver, datasets)
+            return
 
-            password_input = webdriver.find_element_by_xpath(".//*[@id='i0118']")
-            password_input.clear()
+        password_input = webdriver.find_element_by_xpath(".//*[@id='i0118']")
+        password_input.clear()
 
-            # this is required to prevent StaleElementReferenceException
-            actions = ActionChains(webdriver)
-            actions.send_keys("justAnotherPassw0rd!")
-            actions.send_keys(Keys.ENTER)
-            actions.perform()
-            print(f"entered username and password in azure for user: {datasets['username']}")
+        # this is required to prevent StaleElementReferenceException
+        actions = ActionChains(webdriver)
+        actions.send_keys("justAnotherPassw0rd!")
+        actions.send_keys(Keys.ENTER)
+        actions.perform()
+        print(f"entered username and password in azure for user: {datasets['username']}")
 
-            try:
-                # if we don't see password input within 5 seconds ...
-                page.wait_until_visible((By.ID, "idBtn_Back"), 2)
-                stay_signed_in_no = webdriver.find_element_by_xpath(".//*[@id='idBtn_Back']")
-                stay_signed_in_no.click()
-            except TimeoutException:
-                # ... restart test
-                print(f"no stay signed in prompt from azure - about to restart test for user: {datasets['username']}")
-                app_specific_action(webdriver, datasets)
-                return
+        try:
+            # if we don't see password input within 5 seconds ...
+            page.wait_until_visible((By.ID, "idBtn_Back"), 1)
+            stay_signed_in_no = webdriver.find_element_by_xpath(".//*[@id='idBtn_Back']")
+            stay_signed_in_no.click()
+        except TimeoutException:
+            # ... restart test
+            print(f"no stay signed in prompt from azure - about to restart test for user: {datasets['username']}")
+            app_specific_action(webdriver, datasets)
+            return
 
-            """
-            if page.get_elements(LoginPageLocators.continue_button):
-                page.wait_until_visible(LoginPageLocators.continue_button).send_keys(Keys.ESCAPE)
-                page.get_element(LoginPageLocators.continue_button).click()
-                page.wait_until_visible(LoginPageLocators.avatar_page_next_button).click()
-                page.wait_until_visible(LoginPageLocators.explore_current_projects).click()
-                page.go_to_url(DashboardLocators.dashboard_url)
-                page.wait_until_visible(DashboardLocators.dashboard_window)
-            else:
-                page.wait_until_visible((By.ID, "gadget-10002-title"))
-            """
+        """
+        if page.get_elements(LoginPageLocators.continue_button):
+            page.wait_until_visible(LoginPageLocators.continue_button).send_keys(Keys.ESCAPE)
+            page.get_element(LoginPageLocators.continue_button).click()
+            page.wait_until_visible(LoginPageLocators.avatar_page_next_button).click()
+            page.wait_until_visible(LoginPageLocators.explore_current_projects).click()
+            page.go_to_url(DashboardLocators.dashboard_url)
+            page.wait_until_visible(DashboardLocators.dashboard_window)
+        else:
+            page.wait_until_visible((By.ID, "gadget-10002-title"))
+        """
 
-            print(f"before handling post login checks for user: {datasets['username']}")
+        print(f"before handling post login checks for user: {datasets['username']}")
 
-            if login_page.is_first_login():
-                login_page.first_login_setup()
-            if login_page.is_first_login_second_page():
-                login_page.first_login_second_page_setup()
-            login_page.wait_for_page_loaded()
-            webdriver.node_id = login_page.get_node_id()
-            print(f"node_id: {webdriver.node_id}, username: {datasets['username']}")
-
-        sub_measure()
+        if login_page.is_first_login():
+            login_page.first_login_setup()
+        if login_page.is_first_login_second_page():
+            login_page.first_login_second_page_setup()
+        login_page.wait_for_page_loaded()
+        webdriver.node_id = login_page.get_node_id()
+        print(f"node_id: {webdriver.node_id}, username: {datasets['username']}")
     measure()
     PopupManager(webdriver).dismiss_default_popup()
 
