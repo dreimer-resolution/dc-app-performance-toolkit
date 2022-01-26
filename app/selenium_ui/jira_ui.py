c9efd12917272f1a7bf7b8967c658e884d387857
Johannes Borrmann <jobo@newcube.fritz.box>
2022-01-26 10:40:38 +0100

    Removed dead code

--- app/selenium_ui/jira_ui.py
+++ app/selenium_ui/jira_ui.py
@@ -60,15 +60,6 @@ def test_1_selenium_view_project_summary(jira_webdriver, jira_datasets, jira_scr
     modules.view_project_summary(jira_webdriver, jira_datasets)
 
 
-"""
-Add custom actions anywhere between login and log out action. Move this to a different line as needed.
-Write your custom selenium scripts in `app/extension/jira/extension_ui.py`.
-Refer to `app/selenium_ui/jira/modules.py` for examples.
-"""
-# def test_1_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
-#     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
-
-
 # this action should be the last one
 def test_2_selenium_z_log_out(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.log_out(jira_webdriver, jira_datasets)
