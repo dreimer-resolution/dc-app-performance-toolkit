d8ba9d3503c9668c09789d007a8dcac9dcf1d37a
Johannes Borrmann <j.borrmann@resolution.de>
2022-03-31 12:05:45 +0200

    Adjusted selenium test for new User Sync UI

--- app/extension/bitbucket/extension_ui.py
+++ app/extension/bitbucket/extension_ui.py
@@ -22,28 +22,29 @@ def app_specific_action(webdriver, datasets):
         app_specific_user_login(username='admin', password='admin')
     measure()
 
+    """
+    this test is intended to start a new sync, not to measure its duration
+    """
     @print_timing("selenium_app_custom_action")
     def measure():
-
-        @print_timing("selenium_app_custom_action:trigger_usersync")
+        @print_timing("selenium_app_custom_action:start_sync_and_wait_until_complete")
         def sub_measure():
-            # open user sync connector page
-            page.go_to_url(f"{BITBUCKET_SETTINGS.server_url}/plugins/servlet/de.resolution.usersync/admin")
-            # 1 sec pause to prevent 500 errors, obviously caused by trying to use the UI too quickly during automation
-            time.sleep(1)
-            # wait for table to be loaded
-            page.wait_until_visible((By.ID, 'usersync-connector-table-body'))
-            # check if there is a sync running already
-            running_sync = webdriver.find_elements_by_xpath(".//span[.='Sync is running...']")
-            # if not
-            if len(running_sync) == 0:
-                # another check to see if sync button is now available
-                new_sync = webdriver.find_elements_by_xpath(".//span[.='Sync']")
-                if len(new_sync) > 0:
-                    # click on sync
-                    webdriver.find_element_by_xpath(".//span[.='Sync']").click()
-                    # wait for status window
-                    page.wait_until_visible((By.ID, 'usersync-sync-status-dialog'))
-                    # we are done here
+            # open user sync page with one connector added already (set it up so that sync takes just a few seconds)
+            page.go_to_url(f"{BITBUCKET_SETTINGS.server_url}/plugins/servlet/samlsso/usersync")
+            # wait for sync button
+            page.wait_until_visible((By.XPATH, ".//span[text()='Sync']"))
+
+            # check if there is a sync still running, for that we'll fetch the whole reconfigure div text
+            us_div = webdriver.find_element_by_xpath(".//*[@id='reconfigure-react-root']")
+
+            # only try to start sync again if none is still running
+            if "RUNNING" not in us_div.text:
+                # click sync button
+                webdriver.find_element_by_xpath(".//span[text()='Sync']").click()
+                # wait for status window to open
+                page.wait_until_visible((By.XPATH, ".//div[text()='Sync Status']"))
+                # press close button
+                webdriver.find_element_by_xpath(".//span[text()='Close']").click()
         sub_measure()
     measure()
+
