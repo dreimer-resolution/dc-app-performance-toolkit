4a3e5c4c3a5d17c49807bb6aef0984a1050ce317
Johannes Borrmann <j.borrmann@resolution.de>
2022-06-08 15:41:38 +0200

    Reactivated non-custom tests.

--- app/selenium_ui/confluence_ui.py
+++ app/selenium_ui/confluence_ui.py
@@ -6,7 +6,7 @@ from extension.confluence import extension_ui  # noqa F401
 def test_0_selenium_a_login(confluence_webdriver, confluence_datasets, confluence_screen_shots):
     modules.login(confluence_webdriver, confluence_datasets)
 
-"""
+
 def test_1_selenium_view_page(confluence_webdriver, confluence_datasets, confluence_screen_shots):
     modules.view_page(confluence_webdriver, confluence_datasets)
 
@@ -29,7 +29,7 @@ def test_1_selenium_view_blog(confluence_webdriver, confluence_datasets, conflue
 
 def test_1_selenium_view_dashboard(confluence_webdriver, confluence_datasets, confluence_screen_shots):
     modules.view_dashboard(confluence_webdriver, confluence_datasets)
-"""
+
 
 def test_1_selenium_custom_action(confluence_webdriver, confluence_datasets, confluence_screen_shots):
     extension_ui.app_specific_action(confluence_webdriver, confluence_datasets)
