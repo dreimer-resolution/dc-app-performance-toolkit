fd2954f38dc359f1d7f6bb2b32f3bbf9c1342223
Johannes Borrmann <j.borrmann@resolution.de>
2023-01-09 21:43:28 +0100

    Activated test again

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -8,25 +8,25 @@ logger = init_logger(app_type='jira')
 # @run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
 
-    # if 'user_deactivator' not in locust.cross_action_storage or locust.cross_action_storage['user_deactivator'] != 'true':
-    #     # like our test users created with the test connector
-    #     body = {"notInGroups": ["jira-software-users"], "action": "DEACTIVATE"}
-    #
-    #     r = locust.post('/rest/de.resolution.userdeactivator/1.0/ui/users', auth=("admin", "admin"), json=body,  catch_response=True)
-    #     content = r.content.decode('utf-8')   # decode response content
-    #
-    #     if 'resultId' not in content:
-    #         logger.error(f"resultId was not found in {content}, looks like deactivation was not triggered properly")
-    #
-    #     assert 'resultId' in content
-    #
-    #     result_id_pattern = "resultId=(.+?)"
-    #     result_id = re.findall(result_id_pattern, content)
-    #
-    #     logger.locust_info(f"resultId: {result_id[0]}")
-    #     assert int(result_id[0]) >= 0
-    #
-    #     locust.cross_action_storage['user_deactivator'] = 'true'
+    if 'user_deactivator' not in locust.cross_action_storage or locust.cross_action_storage['user_deactivator'] != 'true':
+        # like our test users created with the test connector
+        body = {"notInGroups": ["jira-software-users"], "action": "DEACTIVATE"}
+
+        r = locust.post('/rest/de.resolution.userdeactivator/1.0/ui/users', auth=("admin", "admin"), json=body,  catch_response=True)
+        content = r.content.decode('utf-8')   # decode response content
+
+        if 'resultId' not in content:
+            logger.error(f"resultId was not found in {content}, looks like deactivation was not triggered properly")
+
+        assert 'resultId' in content
+
+        result_id_pattern = "resultId=(.+?)"
+        result_id = re.findall(result_id_pattern, content)
+
+        logger.locust_info(f"resultId: {result_id[0]}")
+        assert int(result_id[0]) >= 0
+
+        locust.cross_action_storage['user_deactivator'] = 'true'
 
     # RESET SESSION USER BACK, UNLESS WE DO THAT WE GET STRANGE
     # "ADMIN STILL LOGGED IN" ERRORS IN OTHER LOCUST ACTIONS
