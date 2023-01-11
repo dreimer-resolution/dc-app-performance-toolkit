3de8b48dc108cf8d6e897e0ae32d79d8ebdbe155
Johannes Borrmann <j.borrmann@resolution.de>
2023-01-11 13:20:46 +0100

    Triggering deactivation only once

--- app/extension/confluence/extension_locust.py
+++ app/extension/confluence/extension_locust.py
@@ -8,20 +8,21 @@ logger = init_logger(app_type='confluence')
 @run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
 
-    # this will trigger deactivation of everybody who is not in confluence-users,
-    # like our test users created with the test connector
-    body = {"notInGroups": ["confluence-users"], "action": "DEACTIVATE"}
+    if 'user_deactivator' not in locust.cross_action_storage or locust.cross_action_storage['user_deactivator'] != 'true':
+        body = {"notInGroups": ["confluence-users"], "action": "DEACTIVATE"}
 
-    r = locust.post('/rest/de.resolution.userdeactivator/1.0/ui/users', json=body, catch_response=True)
-    content = r.content.decode('utf-8')   # decode response content
+        r = locust.post('/rest/de.resolution.userdeactivator/1.0/ui/users', json=body, catch_response=True)
+        content = r.content.decode('utf-8')   # decode response content
 
-    if 'resultId' not in content:
-        logger.error(f"resultId was not found in {content}, looks like deactivation was not triggered properly")
+        if 'resultId' not in content:
+            logger.error(f"resultId was not found in {content}, looks like deactivation was not triggered properly")
 
-    assert 'resultId' in content
+        assert 'resultId' in content
 
-    result_id_pattern = "resultId=(.+?)"
-    result_id = re.findall(result_id_pattern, content)
+        result_id_pattern = "resultId=(.+?)"
+        result_id = re.findall(result_id_pattern, content)
 
-    logger.locust_info(f"resultId: {result_id[0]}")
-    assert int(result_id[0]) >= 0
+        logger.locust_info(f"resultId: {result_id[0]}")
+        assert int(result_id[0]) >= 0
+        locust.cross_action_storage['user_deactivator'] = 'true'
+        
