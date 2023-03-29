ff3a0aa4e1f1805146d04e05da21602875a9f785
Johannes Borrmann <j.borrmann@resolution.de>
2023-03-29 10:00:39 +0200

    Fixed SSO app specific login for Jira and removed dead code.

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -15,33 +15,11 @@ def app_specific_action(webdriver, datasets):
     @print_timing("selenium_app_specific_login")
     def measure():
         login_page = Login(webdriver)
-        #
-        # @print_timing("selenium_app_specific_login:open_login_page")
-        # def sub_measure():
-        #     login_page.go_to()
-        #     webdriver.node_id = login_page.get_node_id()
-        #     print(f"node_id:{webdriver.node_id}")
-        # sub_measure()
 
         @print_timing("selenium_app_specific_login:login_and_view_dashboard")
         def sub_measure():
             print(f"login_with_saml_sso, user: {datasets['username']}")
-            # trigger sso directly
-            # page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/samlsso?redirectTo=%2Fsecure%2FDashboard.jspa")
             page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/samlsso?NameID=" + datasets['username'])
-
-            """
-            # wait for nameID input field to be shown
-            page.wait_until_visible((By.ID, "nameID"))
-            # get field object
-            username_input = webdriver.find_element_by_xpath(".//*[@id='nameID']")
-            # clear existing value
-            username_input.clear()
-            # add username to it
-            username_input.send_keys(datasets['username'])
-            # click send button
-            webdriver.find_element_by_xpath(".//*[@class='btn btn-default']").click()
-            """
 
             # wait for html body id "jira" which is always present, both for users who never logged in and who did
             page.wait_until_visible((By.ID, "jira"))
