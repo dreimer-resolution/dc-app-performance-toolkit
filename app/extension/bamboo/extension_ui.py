c09d9dedd1a6c0c5e6ca93e74c6ed3e34b60e684
Johannes Borrmann <j.borrmann@resolution.de>
2024-01-08 21:16:38 +0100

    Adjusted base url and webdriver method calls

--- app/extension/bamboo/extension_ui.py
+++ app/extension/bamboo/extension_ui.py
@@ -16,8 +16,7 @@ def app_specific_logout(webdriver, datasets):
     def measure():
         page.go_to_url(f"{BAMBOO_SETTINGS.server_url}/userLogout.action")
         pick_signed_in_user \
-            = webdriver.find_elements_by_xpath(".//div[@class='table-cell text-left content']")
-        # only if there is a list of users to be logged out to select from. otherwise, we have been logged out already
+            = webdriver.find_elements("xpath", ".//div[@class='table-cell text-left content']")
         if len(pick_signed_in_user) > 0:
             pick_signed_in_user[0].click()
     measure()
@@ -65,13 +64,13 @@ def app_specific_action(webdriver, datasets):
         # log in with Azure AD - wait for azure user input field to be shown
         page.wait_until_visible((By.ID, "i0116"))
         # get username field
-        username_input = webdriver.find_element_by_xpath(".//*[@id='i0116']")
+        username_input = webdriver.find_element("xpath", ".//*[@id='i0116']")
         # clear existing value
         username_input.clear()
         # add username to it
         username_input.send_keys(datasets['username'] + "@azuread.lab.resolution.de")
         # username_input.send_keys("performance_user_bavba@azuread.lab.resolution.de")
-        next_is_password = webdriver.find_element_by_xpath(".//*[@id='idSIButton9']")
+        next_is_password = webdriver.find_element("xpath", ".//*[@id='idSIButton9']")
         next_is_password.click()
 
         try:
@@ -82,7 +81,7 @@ def app_specific_action(webdriver, datasets):
             app_specific_action(webdriver, datasets)
             return
 
-        password_input = webdriver.find_element_by_xpath(".//*[@id='i0118']")
+        password_input = webdriver.find_element("xpath", ".//*[@id='i0118']")
         password_input.clear()
 
         # this is required to prevent StaleElementReferenceException
@@ -91,7 +90,7 @@ def app_specific_action(webdriver, datasets):
         actions.send_keys(Keys.ENTER)
         actions.perform()
 
-        stay_signed_in_no = webdriver.find_element_by_xpath(".//*[@id='idBtn_Back']")
+        stay_signed_in_no = webdriver.find_element("xpath", ".//*[@id='idBtn_Back']")
         stay_signed_in_no.click()
 
         # wait for build plan dashboard table
