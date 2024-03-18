2a74ebf0a44f2c304c5683ad56d24e4747ec1bfc
Johannes Borrmann <j.borrmann@resolution.de>
2024-03-18 09:46:45 +0100

    Test adjustments to not create zillions of tickets

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -11,6 +11,8 @@ logger = init_logger(app_type='jira')
 def app_specific_action(locust):
 
     headers = {'content-type': 'application/json'}
+    """
+    # customfield_10105 = sprint reference
     create_issue_payload = {
         "fields": {
             "project":
@@ -33,20 +35,25 @@ def app_specific_action(locust):
     logger.locust_info(f'*********** POST Issue: {str(create_end - create_start)} seconds')
     resp_json = r.json()
     issue_key = resp_json['key']
+    """
 
-    put_start = time.time()
-    body = '{"fields": {"customfield_10106": ' + str(int(random.randint(10, 50))) + '}}'
+    issue_key = str(int(random.randint(23634, 23841)))
+    story_points = str(int(random.randint(5, 30)))
+
+    put_story_points_start = time.time()
+    body = '{"fields": {"customfield_10106": ' + story_points + '}}'
     r = requests.put('https://jira.dc-testing.reslab.de/rest/api/2/issue/' + issue_key, body, headers=headers, auth=("admin", "admin"))
-    put_end = time.time()
-    logger.locust_info(f'*********** PUT Issue: {str(put_end - put_start)} seconds')
+    put_story_points_end = time.time()
+    logger.locust_info(f'*********** PUT Issue: {str(put_story_points_end - put_story_points_start)} seconds')
     assert r.status_code == 204
 
+    """
     # delete issue again to not blow up the sprint
     delete_start = time.time()
     r = requests.delete('https://jira.dc-testing.reslab.de/rest/api/2/issue/' + issue_key, headers=headers, auth=("admin", "admin"))
     delete_end = time.time()
     logger.locust_info(f'*********** DEL Issue ({str(r.status_code)}): {str(delete_end - delete_start)} seconds')
-
+    """
 
 
 
