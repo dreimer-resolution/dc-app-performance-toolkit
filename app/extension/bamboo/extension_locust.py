a1515db4ab7cd1e529dc547e917d0b80e3acd62b
Johannes Borrmann <j.borrmann@resolution.de>
2025-11-19 20:05:09 +0100

    since run as app specific user doesn't work, let's use auth for that directly

--- app/extension/bamboo/extension_locust.py
+++ app/extension/bamboo/extension_locust.py
@@ -5,11 +5,13 @@ logger = init_logger(app_type='bamboo')
 
 
 @bamboo_measure("locust_app_specific_action")
-@run_as_specific_user(username='admin', password='admin')
+# @run_as_specific_user(username='admin', password='admin')
 def app_specific_action(locust):
+    username = "admin"
+    password = "admin"
     headers = {'content-type': 'application/json'}
     r = locust.post('/rest/samlsso-admin/1.0/usersync/connector/008eee20-023d-40aa-a941-34be5084c927/sync',
-                    headers, catch_response=True)
+                    headers, auth=(username, password), catch_response=True)
     logger.info(f'Started User Sync Job')
     content = r.content.decode('utf-8')
     assertion_string = 'Scheduled with jobid'
