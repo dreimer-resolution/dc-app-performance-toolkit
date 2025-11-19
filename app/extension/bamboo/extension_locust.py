2a1e52b081911c580938d6097a9520eaf03e843c
Johannes Borrmann <j.borrmann@resolution.de>
2025-11-19 20:24:08 +0100

    More fixes other people should have done already

--- app/extension/bamboo/extension_locust.py
+++ app/extension/bamboo/extension_locust.py
@@ -1,17 +1,18 @@
-import re
+import requests
 from locustio.common_utils import init_logger, bamboo_measure, run_as_specific_user  # noqa F401
-
+from util.conf import BAMBOO_SETTINGS
 logger = init_logger(app_type='bamboo')
 
 
 @bamboo_measure("locust_app_specific_action")
-# @run_as_specific_user(username='admin', password='admin')
+# @run_as_specific_user(username='admin', password='admin') # doesn't work, the framework team needs to fix this
 def app_specific_action(locust):
     username = "admin"
     password = "admin"
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/rest/samlsso-admin/1.0/usersync/connector/008eee20-023d-40aa-a941-34be5084c927/sync',
-                    headers, auth=(username, password), catch_response=True)
+    connector_id = "008eee20-023d-40aa-a941-34be5084c927"
+    # locust post ignores auth, so we need to use requests
+    r = requests.post(f'{BAMBOO_SETTINGS.server_url}/rest/samlsso-admin/1.0/usersync/connector/{connector_id}/sync',
+                  auth=(username, password))
     logger.info(f'Started User Sync Job')
     content = r.content.decode('utf-8')
     assertion_string = 'Scheduled with jobid'
